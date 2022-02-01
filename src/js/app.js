b249970fc12d9523f09633664ed71286c766fa20
Grigoriy Kozheletov <misteryrus@gmail.com>
2022-02-01 21:21:54 +0300

    make header for mobile and desktop

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
-const a = 10;
+const burgerMenu = document.querySelector('.burger-menu');
+const mainNavList = document.querySelector('.main-nav-list');
+
+burgerMenu.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (mainNavList.classList.contains('show')) {
+    mainNavList.classList.remove('show');
+  } else {
+    mainNavList.classList.add('show');
+  }
+});
